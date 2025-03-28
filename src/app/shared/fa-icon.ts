55a5cb173e88bbab5992c45e0d63c5927be32231
charlieTai <charlie.tai@tpisoftware.com>
2025-03-29 04:17:29 +0800

    add : install font awesome

---
+++ src/app/shared/fa-icon.ts
@@ -0,0 +1,17 @@
+import {
+  faThreads,
+  faFacebook,
+  faYoutube,
+  faLinkedin,
+  faLine,
+} from '@fortawesome/free-brands-svg-icons';
+
+export const FooterIcons = {
+  faThreads,
+  faFacebook,
+  faYoutube,
+  faLinkedin,
+  faLine,
+};
+
+export const HeaderIcons = {}
