0255582b1e7e7909d06cabf41d7b3a03a19d8514
charlieTai <charlie.tai@tpisoftware.com>
2025-03-29 03:35:08 +0800

    add : build base i18n

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,22 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateLoad } from './config/translate.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi()),
+    TranslateModule.forRoot({
+      defaultLanguage: 'zh-Hant',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: TranslateLoad,
+        deps: [HttpClient]
+      }
+    }).providers!
+  ]
 };
