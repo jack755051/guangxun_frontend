2d4d8237b2441fe06198216ca6f598fd3e5b7d81
charlieTai <charlie.tai@tpisoftware.com>
2025-04-06 11:33:50 +0800

    add : build base i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
-  selector: 'guangxun_2025-root',
+  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'base_template';
+
+  constructor(private _translate: TranslateService) {
+    // 定義支援的語系（一定要和檔名一致）
+    const supportedLangs = ['en', 'zh-Hant'];
+
+    this._translate.addLangs(supportedLangs);
+    this._translate.setDefaultLang('zh-Hant');
+
+    // 偵測瀏覽器語言（如 zh-TW、zh-CN 等）
+    const browserLang = this._translate.getBrowserLang();
+
+    // 根據 zh 開頭的語言都轉為 zh-Hant，其它則為 en
+    const matchedLang = browserLang?.startsWith('zh') ? 'zh-Hant' : 'en';
+
+    this._translate.use(matchedLang);
+  }
+
+  //Switch system language
+  switchLanguage(lang: string) {
+    this._translate.use(lang);
+  }
 }
--- src/app/config/translate.config.ts
+++ src/app/config/translate.config.ts
@@ -1,7 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { forkJoin, map, Observable } from 'rxjs';
+
+export class CustomTranslateLoader implements TranslateLoader {
+  constructor(private http: HttpClient) {}
+
+  getTranslation(lang: string): Observable<any> {
+    // Map 'zh-Hant' to the correct path for Traditional Chinese
+    const langPath = lang === 'zh-Hant' ? 'zh-Hant' : lang;
+
+    return forkJoin([
+      this.http.get(`./assets/i18n/${langPath}/common.json`),
+      this.http.get(`./assets/i18n/${langPath}/footer.json`),
+      this.http.get(`./assets/i18n/${langPath}/navbar.json`),
+    ]).pipe(
+      map(([common, footer, navbar]) => ({
+        ...common,
+        ...footer,
+        ...navbar,
+      })),
+    );
+  }
+}
 
 export function TranslateLoad(http: HttpClient): TranslateLoader {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new CustomTranslateLoader(http);
 }
